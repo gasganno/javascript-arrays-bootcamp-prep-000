10d82204ac9a7e17e76ae736cb48788b6b7ecd2f
Jarrod Dyer <jarrod@gasganno.com>
2018-09-11 23:35:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 
-
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1)
+  return newArray
+}
 
 
 
